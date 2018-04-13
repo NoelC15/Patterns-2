a8c543808a9d7bdbf7aa1a65cbb753b9c7be1845
Alsolami <malsolami@luc.edu>
2018-04-13 04:26:37 -0500

    implemented evaluator visitor

--- src/Client.java
+++ src/Client.java
@@ -2,9 +2,13 @@
 public class Client {
 	public static void main(String[] args) {
 		Expr test = new Plus(new Constant(1), new Minus(new Constant(3), new UMinus(new Constant(12))));
-//		System.out.println(test.toString());
-		ExprVisitor v = new Printer();
-		String result = (String) test.accept(v);
-		System.out.println(result);
+
+		ExprVisitor prettyPrinter = new Printer();
+		String PrintingResult = (String) test.accept(prettyPrinter);
+		System.out.println(PrintingResult);
+		
+		ExprVisitor accurateEvaluator = new Evaluator();
+		int evaluationResult = (int) test.accept(accurateEvaluator);
+		System.out.println(evaluationResult);
 	}
 }
---
+++ src/Evaluator.java
@@ -0,0 +1,51 @@
+
+public class Evaluator implements ExprVisitor<Integer> {
+
+	@Override
+	public Integer visit(Plus plus) {
+		int lval = plus.getLeft().accept(this);
+		int rval = plus.getRight().accept(this);
+		return lval + rval;
+	}
+
+	@Override
+	public Integer visit(Minus minus) {
+		int lval = minus.getLeft().accept(this);
+		int rval = minus.getRight().accept(this);
+		return lval - rval;
+	}
+
+	@Override
+	public Integer visit(Times times) {
+		int lval = times.getLeft().accept(this);
+		int rval = times.getRight().accept(this);
+		return lval * rval;
+	}
+
+	@Override
+	public Integer visit(Div div) {
+		int lval = div.getLeft().accept(this);
+		int rval = div.getRight().accept(this);
+		return lval / rval;
+	}
+
+	@Override
+	public Integer visit(Mod mod) {
+		int lval = mod.getLeft().accept(this);
+		int rval = mod.getRight().accept(this);
+		return lval % rval;
+	}
+
+	@Override
+	public Integer visit(UMinus uminus) {
+		int val = uminus.getValue().accept(this);
+		return (-val);
+	}
+
+	@Override
+	public Integer visit(Constant constant) {
+		int val = constant.getNumber();
+		return val;
+	}
+
+}
