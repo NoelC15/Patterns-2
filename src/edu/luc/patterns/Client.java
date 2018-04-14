9cf44f4886059a68e6e59a4e8411a64a54b92efa
Mustafa Alsoalmi <malsolami@luc.edu>
2018-04-13 20:30:07 -0500

    updated the expression in Clinet.java again

--- src/edu/luc/patterns/Client.java
+++ src/edu/luc/patterns/Client.java
@@ -2,27 +2,46 @@ package edu.luc.patterns;
 
 public class Client {
 	public static void main(String[] args) {
-//		expr =  (-4 * 2) + (20 % 6) = - 6
-		Expr expr = new Plus(
-							new Times(
-								new UMinus(
-											new Constant(4)
-										  ),
-								new Constant(2)
-							),
-							new Mod(
-									new Constant(20),
-									new Constant(6)
-							)
+
+		Expr p = 
+				new Plus(
+		 		  new Times(
+					new UMinus(
+					  new Constant(4)
+					  ),
+						new Constant(2)
+						),
+						new Mod(
+						  new Constant(20),
+						  new Constant(6)
+						)
 					);
+	    Expr q =
+	            new Mod(
+	              new Minus(
+	                new Plus(
+	                  new Constant(1),
+	                  new Constant(2)
+	                ),
+	                new Times(
+	                  new UMinus(
+	                    new Constant(3)
+	                  ),
+	                  new Constant(4)
+	                )
+	              ),
+	              new Constant(5)
+	            );
+	    Expr complex = new Plus(p, q);
 								
-//		TODO Try with multiple different expressions 
 		ExprVisitor<String> prettyPrinter = new Printer();
-		String PrintingResult = (String) expr.accept(prettyPrinter);
+		String PrintingResult = (String) complex.accept(prettyPrinter);
 		System.out.print("The expression " + PrintingResult);
 		
 		ExprVisitor<Integer> accurateEvaluator = new Evaluator();
-		Integer evaluationResult = (Integer) expr.accept(accurateEvaluator);
+		Integer evaluationResult = (Integer) complex.accept(accurateEvaluator);
 		System.out.println(" evaluates to " + evaluationResult);
+		
+		
 	}
 }
